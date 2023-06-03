20e7be3e7218ff1403adce3463048c20092d26b9
DAAN <daanmgneni@gmail.com>
2023-06-03 08:22:25 +0530

    Created ASP.Net core using Entity Framework Core’s Code first approach

---
+++ DataLayer/FunDoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class FunDoContext: DbContext
+    {
+        public FunDoContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<UserEntity> UserTable { get; set; }
+    }
+}
